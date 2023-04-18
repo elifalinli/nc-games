6e292c29b13d459452c92716704c895418560e9b
elifalinli <semraelifalinli@gmail.com>
2023-04-18 12:26:52 +0100

    add a single review page

---
+++ src/components/SingleReview.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { fetchSingleReview } from "../api";
+import { useParams } from "react-router-dom";
+
+
+export const SingleReview = () => {
+const [review, setReview] = useState([])    
+const params = useParams();
+const id = params.review_id
+console.log(id)
+const [isLoading, setIsLoading] = useState(true);
+
+useEffect(() => {
+    setIsLoading(true);
+    fetchSingleReview(id).then((review) => {
+      setReview(review);
+      console.log(review)
+      setIsLoading(false);
+    });
+  }, []);
+
+
+
+  if(isLoading){
+    return "Loading..."
+  }
+
+  const reviewDate = new Date(review.created_at);
+  const year = reviewDate.getFullYear();
+  const month = reviewDate.getMonth() + 1; // getMonth() returns 0-11, so we add 1
+  const day = reviewDate.getDate();
+return(
+    <main className="single-review">
+    <h2>{review.title}</h2>
+    <h3> “Designed by {review.designer}, in <em>{review.category} </em> category  ”</h3>
+    <h4> Posted at <em> {day}/{month}/{year}</em> <br/> By <em> {review.owner} </em> </h4>
+    <img src={review.review_img_url} alt="a representation of the game"/>
+    <p>{review.review_body}</p>
+    </main>
+)
+}
